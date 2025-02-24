c36976ce91b90b50c32a1dcb40a904cf7fcf6697
trajanoo <brunotrajano.dev@gmail.com>
2025-02-23 21:27:25 -0300

    feat: model adicionado

---
+++ src/main/java/com/habittracker/HabitTracker/Habit/HabitModel.java
@@ -0,0 +1,41 @@
+package com.habittracker.HabitTracker.Habit;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "habits")
+@AllArgsConstructor
+@NoArgsConstructor
+public class HabitModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String habitName;
+    private String habitDescription;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHabitName() {
+        return habitName;
+    }
+
+    public void setHabitName(String habitName) {
+        this.habitName = habitName;
+    }
+
+    public String getHabitDescription() {
+        return habitDescription;
+    }
+
+    public void setHabitDescription(String habitDescription) {
+        this.habitDescription = habitDescription;
+    }
+}
